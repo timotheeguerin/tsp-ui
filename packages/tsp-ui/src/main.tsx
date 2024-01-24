02f15b6daa9470105ef29f1a68e549e4273d67a4
Timothee Guerin <tiguerin@microsoft.com>
2024-01-23 17:49:02 -0800

    Setup playground

--- packages/tsp-ui/src/main.tsx
+++ packages/tsp-ui/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { registerMonacoDefaultWorkersForVite } from "@typespec/playground";
+import PlaygroundManifest from "@typespec/playground/manifest";
+import {
+  renderReactPlayground,
+} from "@typespec/playground/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "@typespec/playground/style.css";
+
+registerMonacoDefaultWorkersForVite();
+
+
+await renderReactPlayground({
+  ...PlaygroundManifest,
+  emitterViewers: {
+    "@typespec/openapi3": [],
+  },
+});
--- packages/tsp-ui/vite.config.ts
+++ packages/tsp-ui/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { definePlaygroundViteConfig } from "@typespec/playground/vite";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(() => {
+  const config = definePlaygroundViteConfig({
+    defaultEmitter: "@typespec/openapi3",
+    libraries: [
+      "@typespec/compiler",
+      "@typespec/http",
+      "@typespec/rest",
+      "@typespec/openapi",
+      "@typespec/openapi3",
+      "@typespec/versioning"
+    ],
+    links: {
+      documentationUrl: "https://microsoft.github.io/typespec",
+    },
+  });
+  return config;
+});
